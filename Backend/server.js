e2ee7f8ddbbbd4ada941e55ca25efba1661920ea
mouhamed dia <saotof@gmail.com>
2026-04-25 12:36:32 +0000

    My Login

--- Backend/server.js
+++ Backend/server.js
@@ -20,4 +20,4 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "../")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
-});
+}) ;
