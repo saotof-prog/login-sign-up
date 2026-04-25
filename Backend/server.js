b732d819c3bf3ec7b4ec54da41a562ac223c16b0
mouhamed dia <saotof@gmail.com>
2026-04-25 12:25:40 +0000

    My Login

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const cors = require('cors');
+
+const app = express();
+
+app.use(session({ secret: 'your_secret', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ...add routes for each provider here...
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.get("/", (req, res) => {
+  res.send("Backend connecté !");
+});
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
--- script.js
+++ script.js
@@ -17,3 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+function login() {
+  const email = document.querySelector("input[type='email']").value;
+  const password = document.querySelector("input[type='password']").value;
+
+  fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, password })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.message));
+}
